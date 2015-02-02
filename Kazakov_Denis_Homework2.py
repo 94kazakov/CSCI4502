05f45099271f167f39c4506f88f8fe46ac085a43
user <94kazakov@gmail.com>
2015-02-02 12:39:46 -0700

    regex still doesn't work

--- Kazakov_Denis_Homework2.py
+++ Kazakov_Denis_Homework2.py
@@ -11,10 +11,10 @@ for i,dataLine in enumerate(data):
 	if (i>2):
 		data_rows.append(dataLine.split(','))
 print data_rows [2]
-regex = re.compile(r"^\"[0-9]*\.[0-9]*\"",re.IGNORECASE)
+#regex = re.compile(r"^\"[0-9]*\.[0-9]*\"",re.IGNORECASE)
 for line in data_rows:
 	for word in line:
-		word = regex.sub(r'\1', word)
+		word = re.sub(r"^\"[0-9]*\.[0-9]*\"",'\1', word)
 		print word
 print data_rows [2]
 columns = zip(*data_rows) #{1,2,3} + {4,5,6} => {(1,4),(2,5),(3,6)}
